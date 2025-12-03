2a6f4b79ba8f0756fe8682fa627b3f4233c69f8f
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-12-03 15:20:54 +0200

    modified save code

--- scripts/globals/GameManager.gd
+++ scripts/globals/GameManager.gd
@@ -1,6 +1,21 @@
 extends Node
 
-## Save configs to separate cfg/ini file.
-## Choose save slot depending on current loaded save. If in menu, then no need to save data anyways.
+const PASS_KEY = "fhdiuawyfhioaw901279172"
+
+var save_path = "user://game.save"
+var save_data = {
+    "test" = 1
+}
+
+## Save configs to separate cfg/ini file. TODO
+## Choose save slot depending on current loaded save. If in menu, then no need to save data anyways. TODO
 func save_game() -> void:
-    pass
+    var file := FileAccess.open_encrypted_with_pass(save_path, FileAccess.WRITE, PASS_KEY)
+    file.store_string(JSON.stringify(save_data))
+    
+func load_game() -> void:
+    var file := FileAccess.open_encrypted_with_pass(save_path, FileAccess.READ, PASS_KEY)
+    if file == null:
+        # opened incorrectly
+        return
+    var save_data_loaded: String = file.get_as_text()    
